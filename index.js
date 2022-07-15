20a57bfe0635d21731df6e7a1477cfd61707d4ce
karina2190 <108882554+karina2190@users.noreply.github.com>
2022-07-15 09:41:45 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
 var num = Math.floor(Math.random() * 3) + 1; 
-function pressedButton1() {
-    if (num == 1) {
-        alert('winner');
-    } else {
-        alert('loser');
-    }
-    num = Math.floor(Math.random() * 3) + 1; 
-}
-
-function pressedButton2() {
-    if (num == 2) {
-        alert('winner');
-    } else {
-        alert('loser');
-    }
-    num = Math.floor(Math.random() * 3) + 1; 
-}
-
-function pressedButton3() {
-    if (num == 3) {
+function pressedButton(id) {
+    if (num == id) {
         alert('winner');
     } else {
         alert('loser');
